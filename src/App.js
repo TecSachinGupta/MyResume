45615e811d5e14ec6cc15dd1ce72d615c2f4e352
Sachin Gupta <sachin-gu@hcl.com>
2020-02-21 17:54:44 +0530

    base for the CV...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contact from './components/contact';
+import Education from './components/education';
+import Experience from './components/education';
+import Header from './components/header';
+import Heading from './components/education';
+import Skills from './components/education';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<div className="container">
+			
+			<div className="columns">
+				<div className="column col-12">
+					<Header></Header>
+				</div>
+			</div>
+			
+			<div className="columns">
+				<div className="column col-5">
+					
+				</div>
+				<div className="column col-7">
+				
+				</div>
+			</div>
+		</div>
     </div>
   );
 }
