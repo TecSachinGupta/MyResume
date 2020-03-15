1235016b9a0a25dadc4351ea7bf6be39b13a44d6
Sachin Gupta <sachingupta01@live.com>
2020-03-15 23:22:15 +0530

    Some UI updates for the skills...

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -3,12 +3,15 @@ import React from 'react';
 class Contact extends React.Component {
 	render() {
 		return (
-			<ul>
-				<li><span className=""></span> +91 - 9560247465 </li>
-				<li><span className=""></span> sachingupta01@live.com </li>
-				<li><span className=""></span> sachingupta.me </li>
-				<li><span className=""></span> RZF-754/10 Street No. 2 Rajnagar part 2 Main Railway Road New Delhi - 110077 </li>
-			</ul>
+			<React.Fragment>
+				<div className="componentTitleLeftSidebar"><h4>Contact</h4></div>
+				<ul>
+					<li><span className=""></span> +91 - 9560247465 </li>
+					<li><span className=""></span> sachingupta01@live.com </li>
+					<li><span className=""></span> sachingupta.me </li>
+					<li><span className=""></span> RZF-754/10 Street No. 2 Rajnagar part 2 Main Railway Road New Delhi - 110077 </li>
+				</ul>
+			</React.Fragment>	
 		);
 	}
 }
--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import Icons from '../icons';
+// import Icons from '../icons';
 
 class Skills extends React.Component {
 	render() {
+		const data = this.props.skills.sets.map(set => {
+			return (
+				<li>
+					<div className="skillsTitleSet">{set.name}</div>
+					<div className="subSkillsDetails">
+						<ul className="">
+							{set.skills.map(skill => {
+								return (
+									<li>
+										{skill.name}
+										<div className="bar bar-sm">
+										<div class="bar-item" role="progressbar" style={{"width": `${skill.level * 10}%`}} aria-valuenow={skill.level} aria-valuemin="0" aria-valuemax="10"></div>
+										</div>
+									</li>
+								)
+							})}
+						</ul>
+					</div>
+				</li>
+			)
+		});
 		return (
-			<ul className="skillsDetails">
-				<li><span><Icons icon="jigsaw" size="32" /></span></li>
-				<li><span><Icons icon="CPLUSPLUS" /></span></li>
-				<li><span><Icons icon="C" /></span></li>
-				<li><span><Icons icon="C" /></span></li>
-				<li><span><Icons icon="C" /></span></li>
-				<li><span><Icons icon="C" /></span></li>
-				<li><span><Icons icon="C" /></span></li>
-				<li><span><Icons icon="C" /></span></li>
-			</ul>
+			<React.Fragment>
+				<div className="componentTitleLeftSidebar"><h4>Professional Skills</h4></div>
+				<div className="skillsDetails">
+					<ul className="">
+						{data}
+					</ul>
+				</div>
+			</React.Fragment>
 		);
 	}
 }
