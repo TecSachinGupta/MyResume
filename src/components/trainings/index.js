92ae0462310a2dbbd0699ec212f95f1f1b22cde0
Sachin Gupta <sachingupta01@live.com>
2020-03-28 00:49:18 +0530

    UI and data Update...

---
+++ src/components/trainings/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Trainings extends React.Component {
+	render() {
+		const data = this.props.trainings.history.map((employment, index) => {
+			return (
+				<li className="timeline-event" key={`experience-${index}`}>
+					<label className="timeline-event-icon"></label>
+					<div className="timeline-event-copy">
+					<p className="timeline-event-thumbnail">{`${employment.start} - ${employment.end ? employment.end : 'Till date' } `}</p>
+					<h3>{`${employment.employer}, ${employment.state}, ${employment.country}`}</h3>
+					<h4>{employment.position}</h4>
+					<p>{employment.summary}</p>
+					</div>
+				</li>
+			)
+		});
+		return (
+			<React.Fragment>
+				<div className="componentTitle">
+					<h1>{this.props.children} Experience</h1>
+				</div>
+				<div className="timeline-2">
+					<ul className="timeline">
+						{data}
+					</ul>  
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default Trainings;
